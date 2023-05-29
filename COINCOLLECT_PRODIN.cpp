dbc8c8670ffc87be6e51251d8014edc3d95a3a5f
Silvio Leite <75496033+srgmleite@users.noreply.github.com>
2023-05-29 15:23:58 -0300

    Update COINCOLLECT_PRODIN.cpp

--- COINCOLLECT_PRODIN.cpp
+++ COINCOLLECT_PRODIN.cpp
@@ -4,32 +4,32 @@ using namespace std;
 
 const int INF = -1e9;
 
-int coinCollecting(vector<vector<int>>& grid) {
+int cataMoeda(vector<vector<int>>& grid) {
     int n = grid.size();
     int m = grid[0].size();
-    vector<vector<int>> dpTable(n, vector<int>(m, INF));
-    dpTable[0][0] = grid[0][0];
+    vector<vector<int>> dpTabela(n, vector<int>(m, INF));
+    dpTabela[0][0] = grid[0][0];
 
     for (int j = 1; j < m; j++) {
-        dpTable[0][j] = dpTable[0][j - 1] + grid[0][j];
+        dpTabela[0][j] = dpTabela[0][j - 1] + grid[0][j];
     }
 
     for (int i = 1; i < n; i++) {
-        dpTable[i][0] = dpTable[i - 1][0] + grid[i][0];
+        dpTabela[i][0] = dpTabela[i - 1][0] + grid[i][0];
     }
 
     for (int i = 1; i < n; i++) {
         for (int j = 1; j < m; j++) {
-            dpTable[i][j] = max(dpTable[i - 1][j], dpTable[i][j - 1]) + grid[i][j];
+            dpTabela[i][j] = max(dpTabela[i - 1][j], dpTabela[i][j - 1]) + grid[i][j];
         }
     }
 
-    return dpTable[n - 1][m - 1];
+    return dpTabela[n - 1][m - 1];
 }
 
 int main() {
     int n, m;
-    cout << "Insira o número de linhas e colunas: ";
+    cout << "Insira o nÃºmero de linhas e colunas: ";
     cin >> n >> m;
 
     vector<vector<int>> grid(n, vector<int>(m));
@@ -40,8 +40,8 @@ int main() {
         }
     }
 
-    int maxCoins = coinCollecting(grid);
-    cout << "O número máximo de moedas coletadas foi: " << maxCoins << endl;
+    int maxMoedas = cataMoeda(grid);
+    cout << "O nÃºmero mÃ¡ximo de moedas coletadas foi: " << maxMoedas << endl;
 
     return 0;
 }
